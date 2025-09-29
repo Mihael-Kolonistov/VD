d42ba6a929b3f0cd015e8995378edeeabce9908d
Mihael-Kolonistov <gbnjy19738246@gmail.com>
2025-09-29 19:16:54 +0300

    gg

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from sender import Snd
+import json
 
 chat = Snd('192.168.56.1')
 
+msg = json.load(open("message.json", "r", encoding="utf-8"))
 while True:
     text = input()
     
-    chat.send()
+    msg["content"]["text"] = text
+    
+    chat.send(msg)
