ff6cae3597906766163320c5b08e983d7743e62c
Mihael-Kolonistov <gbnjy19738246@gmail.com>
2025-09-29 22:38:01 +0300

    gg

--- lib.py
+++ lib.py
@@ -1,21 +1,23 @@
-import pyautogui
 import msvcrt
-import threading
-posc=()
-poscBefore=()
-click = False
-pressed=""
-def pos(): 
-    global posc, poscBefore      
-    if posc != poscBefore and posc != ():   
-        x,y = pyautogui.position()
-        poscBefore = posc
-        posc=(x,y)
-    
-def press():  
-    global pressed          
-    char = msvcrt.getch() 
-    pressed = char.decode('utf-8')  
-    
-    pressed = ""
+from pynput import mouse
+from mgs import msgF, msgT, msgC
+class main:
+    def press(self):  
+        
+        global pressed          
+        char = msvcrt.getch() 
+        pressed = char.decode('utf-8')  
+        msgC["key"] = pressed
 
+    def pos(self, x, y):
+        msgC["posx"] = x
+        msgC["posy"] = y
+
+    def cl(self, button, pressed):
+        msgC["click"] =  True if pressed else False
+    def __init__(self):
+        with mouse.Listener(on_move=self.pos, on_click=self.cl) as ls:
+            ls.join()
+if __name__=="__main__":
+    main.__init__(self = main)
+    
--- mgs.py
+++ mgs.py
@@ -1,8 +1,3 @@
-from sender import Snd
-import json
-
-chat = Snd('192.168.56.1')
-
 msgF ={   
     "type": "file",     
     "from": "",
@@ -27,14 +22,3 @@ msgC ={
     "key": ""
     
 }
-
-def init():
-    
-    while True:
-        text = input()
-
-        msgT["content"]["text"] = text
-
-        chat.send(msgT)
-if __name__=="__main__":
-    msgT["content"]["text"] = "Вы подключены!"
