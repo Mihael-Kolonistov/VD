1c24fa7412b706b46e4ac686c33b45d876a73089
Mihael-Kolonistov <gbnjy19738246@gmail.com>
2025-09-29 23:14:47 +0300

    gg

--- main.py
+++ main.py
@@ -1,40 +1,9 @@
 from sender import Snd
 import json
+from mgs import *
 
 chat = Snd('192.168.56.1')
 
-msgF ={   
-    "type": "file",     
-    "from": "",
-    "to": "",
-    "content": {
-        "text": ""
-    },
-    
-}
-
-msgT ={    
-    "type": "text",       
-    "text": ""    
-}
-
-msgC ={   
-    "type": "curBut",  
-
-    "posx": 0,
-    "posy": 0,
-    "click": False,
-    "key": ""
-    
-}
-
-def init():
-    
-    while True:
-        text = input()
-
-        msgT["content"]["text"] = text
-
-        chat.send(msgT)
 if __name__=="__main__":
-    msgT["content"]["text"] = "Вы подключены!"
+    msgT["text"] = "Вы подключены!"
+    chat.send(msgT)
