5628ab2bbe9ed4e2c581cfd1aa332b6f063a413c
Mihael-Kolonistov <gbnjy19738246@gmail.com>
2025-09-30 21:22:01 +0300

    gfg

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 from sender import Snd
 import json
 from mgs import *
+import threading
 
-chat = Snd('192.168.56.1')
+msgCP, msgFP, msgTP = msgC, msgF, msgT
 
-if __name__=="__main__":
-    msgT["text"] = "Вы подключены!"
-    chat.send(msgT)
+snd = Snd('192.168.56.1')
+
+def upd():
+    while True:        
+        global msgF, msgT, msgC, msgCP, msgFP, msgTP
+        if json.dumps(msgC)!=json.dumps(msgCP):
+            snd.send(msgC)
+
+        if json.dumps(msgF)!=json.dumps(msgFP):
+            snd.send(msgF)
+
+        if json.dumps(msgT)!=json.dumps(msgTP):
+            snd.send(msgT)
+
+        msgCP, msgFP, msgTP = msgC, msgF, msgT
+        print(msgC, msgF, msgT)
+        print(msgCP, msgFP, msgTP)
+
+
+
+
+threading.Thread(target=upd).start()
+
+msgT["text"] = "Вы подключены!"
+snd.send(msgT)
+    
