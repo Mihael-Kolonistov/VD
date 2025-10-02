594d302f0cba9d0e99c534e48b1d15684660d016
Mihael-Kolonistov <gbnjy19738246@gmail.com>
2025-10-02 23:04:40 +0300

    ghghgh

--- gui/guiMenu.py
+++ gui/guiMenu.py
@@ -67,10 +67,6 @@ type=LabelFrame(prarm, text="Тип подключения")
 ip=LabelFrame(prarm, text="ip(обязательно при подключении первым)")
 port=LabelFrame(prarm, text="Порт подключения")
 
-type.rowconfigure(index=0, weight=1)
-ip.rowconfigure(index=0, weight=1)
-port.rowconfigure(index=0, weight=1)
-
 types = ["Desktop", "User"]#user = sender, desktop = client
 tps= ttk.Combobox(type, values=types).grid(sticky="ns")
 
