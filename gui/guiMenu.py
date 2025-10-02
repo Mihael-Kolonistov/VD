2aa57c51cf0c41e255b2a6dd156d7c20cbf62654
Mihael-Kolonistov <gbnjy19738246@gmail.com>
2025-10-02 21:57:38 +0300

    dfdfdf

--- gui/guiMenu.py
+++ gui/guiMenu.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, ttk, LabelFrame
+import json
 
 root = Tk()
 
@@ -11,15 +12,26 @@ root.rowconfigure(index=0, weight=1)
 win=LabelFrame(root, text="Старотвые настройки")
 win.rowconfigure(index=0, weight=1)
 
-actions = LabelFrame(win, text="Инструменты")
+actions = LabelFrame(root, text="Инструменты")
 actions.rowconfigure(index=0, weight=1)
 
-asRoot = ttk.Button(actions)
-asRoot = ttk.Button(actions)
+podkl = ttk.Button(actions)
 
 win.grid(sticky="nswe", padx=5, pady=5 , ipadx=5, ipady=5)
 actions.grid(sticky="nswe", padx=5, pady=5 , ipadx=5, ipady=5, column=0, row=1)
 
-asRoot.grid(column=0, row=0)
+podkl.grid(column=0, row=0)
 
+with open("config.json", "r") as cfg:
+    cfgj = json.dump(cfg)
+    if cfgj["therme"] == "night":
+        root["backgroud"]="#242424"
+        win["backgroud"]="#242424"
+        actions["backgroud"]="#242424"
+        podkl["backgroud"]="#242424"
+    elif cfgj["therme"] == "day":
+        root["backgroud"]="#E0E0E0"
+        win["backgroud"]="#E0E0E0"
+        actions["backgroud"]="#E0E0E0"
+        podkl["backgroud"]="#E0E0E0"
 root.mainloop()
